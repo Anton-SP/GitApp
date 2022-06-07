a9f2fbae907a88ff57f1c15f7ae5121df7b7295f
PetrovAS <toha1988cool@gmail.com>
2022-06-07 22:22:16 +0300

    Basic layout imp

--- app/src/main/java/com/home/gitapp/MainActivity.kt
+++ app/src/main/java/com/home/gitapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.home.gitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.home.gitapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainActivityRefreshButton.setOnClickListener {
+            Snackbar.make(binding.root,"Test Click",Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
