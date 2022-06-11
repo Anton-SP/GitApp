90fd91e14d135c24d50791ff043bc5a5d691d8a9
AntonSP <toha1988cool@gmail.com>
2022-06-11 22:04:44 +0300

    HomeWork2 work on mistakes and basic MVP pattern

--- app/src/main/java/com/home/gitapp/ui/MainActivity.kt
+++ app/src/main/java/com/home/gitapp/ui/MainActivity.kt
@@ -7,19 +7,22 @@ import androidx.core.view.isVisible
 import androidx.lifecycle.coroutineScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
-import com.home.gitapp.UsersViewModel
+import com.home.gitapp.app
 import com.home.gitapp.data.NetUserRepoImp
 import com.home.gitapp.databinding.ActivityMainBinding
+import com.home.gitapp.domain.UserEntity
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), UserContract.View {
     private lateinit var binding: ActivityMainBinding
 
     private val adapter = UserAdapter()
 
-    private val userViewModel: UsersViewModel by viewModels{
+    private val userViewModel: UsersViewModel by viewModels {
         UsersViewModel.UsersViewModelFactory(NetUserRepoImp())
     }
+
+    private lateinit var presenter: UserContract.Presenter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,41 +30,53 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initViews()
-        initRecycleView()
+
+        presenter = extractPresenter()
+        presenter.attach(this)
+
+    }
+
+    override fun onRetainCustomNonConfigurationInstance(): UserContract.Presenter? {
+        return presenter
+    }
+
+    private fun extractPresenter(): UserContract.Presenter {
+        return lastCustomNonConfigurationInstance as? UserContract.Presenter
+            ?: UserPresenter(app.userRepo)
     }
 
     private fun initViews() {
         binding.mainActivityRefreshButton.setOnClickListener {
-            loadData()
+            this.lifecycle.coroutineScope.launchWhenStarted {
+                presenter.onRefresh()
+            }
         }
+        initRecycleView()
     }
 
     private fun loadData() {
-        showProgress(true)
         userViewModel.requestUserList()
-        onDataLoaded()
+
     }
 
-    private fun onDataLoaded () {
-        this.lifecycle.coroutineScope.launchWhenStarted {
-            userViewModel.userList.collect(){ userList ->
-                adapter.setData(userList)
-                showProgress(false)
-            }
-        }
-    }
-
-    private fun onError(throwable: Throwable){
-        Snackbar.make(binding.root, throwable.message.toString(), Snackbar.LENGTH_SHORT).show()
-    }
 
     private fun initRecycleView() {
         binding.mainActivityRecycle.layoutManager = LinearLayoutManager(this)
         binding.mainActivityRecycle.adapter = adapter
     }
 
-    private fun showProgress(inProgress:Boolean){
+
+    override fun showUsers(users: List<UserEntity>) {
+        adapter.setData(users)
+
+    }
+
+    override fun showError(throwable: Throwable) {
+        Snackbar.make(binding.root, throwable.message.toString(), Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun showPorgress(inProgress: Boolean) {
         binding.mainActivityProgressBar.isVisible = inProgress
-        binding.mainActivityRecycle.isVisible= !inProgress
+        binding.mainActivityRecycle.isVisible = !inProgress
     }
 }
--- app/src/main/java/com/home/gitapp/ui/UserViewHolder.kt
+++ app/src/main/java/com/home/gitapp/ui/UserViewHolder.kt
@@ -6,12 +6,12 @@ import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.home.gitapp.R
 import com.home.gitapp.domain.UserEntity
-import com.home.gitapp.databinding.UserItemBinding
+import com.home.gitapp.databinding.ItemUserBinding
 
 class UserViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
-    LayoutInflater.from(parent.context).inflate(R.layout.user_item, parent, false)
+    LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent, false)
 ) {
-    private val binding = UserItemBinding.bind(itemView)
+    private val binding = ItemUserBinding.bind(itemView)
 
     fun bind(userEntity: UserEntity) {
         binding.userItemAvatar.load(userEntity.avatarUrl)
